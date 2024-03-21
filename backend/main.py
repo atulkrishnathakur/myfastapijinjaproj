208b34a7ba8797add20082370d61917cc0f02ce1
Atul <atul77.cs@gmail.com>
2024-03-21 11:09:45 +0530

    work with jinja template

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,10 @@
 from apis.base import api_router
+from apps.base import app_router
 from core.config import settings
 from db.base import Base
 from db.session import engine
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 
 # from db.base_class import Base # now do not import
 
@@ -13,12 +15,18 @@ def create_tables():
 
 def include_router(app):
     app.include_router(api_router)
+    app.include_router(app_router)
+
+
+def configure_staticfiles(app):
+    app.mount("/static", StaticFiles(directory="static"), name="static")
 
 
 def start_application():
     app = FastAPI(title=settings.PROJECT_NAME, version=settings.PROJECT_VERSION)
     create_tables()
     include_router(app)
+    configure_staticfiles(app)
     return app
 
 
